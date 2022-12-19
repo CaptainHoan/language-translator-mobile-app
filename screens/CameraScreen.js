b2bfb9cb1d03b9570087506f9a5d18ddbee02c0a
captain <hoancaptain123456789@gmail.com>
2022-12-19 22:39:35 +0700

    add camera

---
+++ screens/CameraScreen.js
@@ -0,0 +1,15 @@
+import { View, Text, StyleSheet } from 'react-native'
+import React from 'react'
+
+const CameraScreen = () => {
+
+  //camera setting
+
+  return (
+    <View>
+
+    </View>
+  )
+}
+
+export default CameraScreen
